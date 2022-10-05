e1d0a6a79dfb8028013666a1c0a20550b1e6107b
tyieshap9 <pattersonty16@gmail.com>
2022-10-05 17:13:04 -0400

    updated

---
+++ js-intro-10-3.js
@@ -0,0 +1,16 @@
+console.log("Hello World")
+
+//variable declariation
+let greetingMessage = 'Hello'
+greetingMessage= 'Bonjour'
+
+const name ='Tyiesha'
+console.log(greetingMessage)
+
+//string
+let myString ='anything can be a string'
+
+//boolean
+let isSleepy = true
+
+//null
---
+++ madlibs.js
@@ -0,0 +1,10 @@
+let adjectives =['adorable', 'annoying', 'copperative']
+let nouns =['airplane','coffee','camera']
+let myAge = 10+18
+//console.log(myAge)
+
+let destination= true
+
+
+shortStory= `She is so ${adjectives[0]}, this is her firs10t time on an ${nouns[0]}. She is ${myAge}. Her brother is so ${adjectives[1]}, all morning he has been drinking ${nouns[1]}. But their is a silver lining, I bought a new ${nouns[2]}. Hopefully this thing is ${adjectives[2]}. When we get to Paris, I hope that everything everybody says is ${destination}.  `
+console.log(shortStory)
