df9526bad74232bed98b9852ce5310c333df6a46
tyieshap9 <pattersonty16@gmail.com>
2022-10-16 18:37:49 -0400

    newly updated

---
+++ adventure.js
@@ -0,0 +1,37 @@
+let fun = prompt('Would you like to go camping or to the beach?')
+fun = 'Lets have fun!'
+
+
+
+if(fun ==='camping'){
+    alert('Your going camping!')
+    let activity = prompt("type 'forest or camping grounds' ")
+
+    if (activity === 'Forest'){
+            prompt('do you want to stay in a rv or tent')
+        }
+    else if ('camping grounds')
+            prompt('do you want to stay in a rv or tent')
+    else{
+        alert('error')
+    }
+}
+
+
+else if(fun === 'beach'){
+    alert('Your going to the beach!')
+    let adventure =prompt('Do want to go to florida or california')
+        
+    if(adventure === 'Florida'){
+            prompt('Do you want to stay in a hotel or beach house')
+    }
+    else if( 'California'){
+        prompt('Do you want to stay in a hotel or beach house')
+    }
+    else{
+        alert('error')
+      
+    }
+}
+console.log(fun)
+
